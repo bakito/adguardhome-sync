450de6e9c70bbb688fadbd59a48d8754db56f848
bakito <github@bakito.ch>
2023-11-12 17:17:03 +0100

    set new min version to v0.107.40

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -4,13 +4,7 @@ import "golang.org/x/mod/semver"
 
 const (
 	// MinAgh minimal adguardhome version
-	MinAgh = "v0.107.0"
-	// IncompatibleAPI adguardhome  version with incompatible API
-	// https://github.com/bakito/adguardhome-sync/issues/99
-	IncompatibleAPI = "v0.107.14"
-	// FixedIncompatibleAPI adguardhome version with fixed API
-	// https://github.com/bakito/adguardhome-sync/issues/99
-	FixedIncompatibleAPI = "v0.107.15"
+	MinAgh = "v0.107.40"
 )
 
 func IsNewerThan(v1 string, v2 string) bool {
