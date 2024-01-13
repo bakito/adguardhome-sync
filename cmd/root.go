2deecca70952c640efb2c52b0fc3296cadf8edeb
bakito <github@bakito.ch>
2024-01-13 10:36:18 +0100

    correct config issues #271 #272

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"regexp"
@@ -111,6 +112,7 @@ func initConfig() {
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".adguardhome-sync")
 	}
+	viper.SetConfigType("yaml")
 	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_", ".", "_"))
 	viper.AutomaticEnv() // read in environment variables that match
 
@@ -128,15 +130,23 @@ func getConfig() (*types.Config, error) {
 	if err := viper.Unmarshal(cfg); err != nil {
 		return nil, err
 	}
+
 	if cfg.Replica != nil &&
 		cfg.Replica.URL == "" &&
 		cfg.Replica.Username == "" {
 		cfg.Replica = nil
 	}
 
-	if len(cfg.Replicas) == 0 {
-		cfg.Replicas = append(cfg.Replicas, collectEnvReplicas()...)
+	if len(cfg.Replicas) > 0 && cfg.Replica != nil {
+		return nil, errors.New("mixed replica config in use. " +
+			"Do not use single replica and numbered (list) replica config combined")
 	}
+
+	if cfg.Replica != nil {
+		cfg.Replicas = []types.AdGuardInstance{*cfg.Replica}
+	}
+
+	cfg.Replicas = enrichReplicasFromEnv(cfg.Replicas)
 
 	handleDeprecatedEnvVars(cfg)
 
@@ -144,70 +154,56 @@ func getConfig() (*types.Config, error) {
 }
 
 func handleDeprecatedEnvVars(cfg *types.Config) {
-	value := checkDeprecatedEnvVar("RUNONSTART", "RUN_ON_START")
-	if value != "" {
-		cfg.RunOnStart, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("RUNONSTART", "RUN_ON_START"); ok {
+		cfg.RunOnStart, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("API_DARKMODE", "API_DARK_MODE")
-	if value != "" {
-		cfg.API.DarkMode, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("API_DARKMODE", "API_DARK_MODE"); ok {
+		cfg.API.DarkMode, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_GENERALSETTINGS", "FEATURES_GENERAL_SETTINGS")
-	if value != "" {
-		cfg.Features.GeneralSettings, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_GENERALSETTINGS", "FEATURES_GENERAL_SETTINGS"); ok {
+		cfg.Features.GeneralSettings, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_QUERYLOGCONFIG", "FEATURES_QUERY_LOG_CONFIG")
-	if value != "" {
-		cfg.Features.QueryLogConfig, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_QUERYLOGCONFIG", "FEATURES_QUERY_LOG_CONFIG"); ok {
+		cfg.Features.QueryLogConfig, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_STATSCONFIG", "FEATURES_STATS_CONFIG")
-	if value != "" {
-		cfg.Features.StatsConfig, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_STATSCONFIG", "FEATURES_STATS_CONFIG"); ok {
+		cfg.Features.StatsConfig, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_CLIENTSETTINGS", "FEATURES_CLIENT_SETTINGS")
-	if value != "" {
-		cfg.Features.ClientSettings, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_CLIENTSETTINGS", "FEATURES_CLIENT_SETTINGS"); ok {
+		cfg.Features.ClientSettings, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_DHCP_SERVERCONFIG", "FEATURES_DHCP_SERVER_CONFIG")
-	if value != "" {
-		cfg.Features.DHCP.ServerConfig, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_DHCP_SERVERCONFIG", "FEATURES_DHCP_SERVER_CONFIG"); ok {
+		cfg.Features.DHCP.ServerConfig, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_DHCP_STATICLEASES", "FEATURES_DHCP_STATIC_LEASES")
-	if value != "" {
-		cfg.Features.DHCP.StaticLeases, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_DHCP_STATICLEASES", "FEATURES_DHCP_STATIC_LEASES"); ok {
+		cfg.Features.DHCP.StaticLeases, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_DNS_ACCESSLISTS", "FEATURES_DNS_ACCESS_LISTS")
-	if value != "" {
-		cfg.Features.DNS.AccessLists, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_DNS_ACCESSLISTS", "FEATURES_DNS_ACCESS_LISTS"); ok {
+		cfg.Features.DNS.AccessLists, _ = strconv.ParseBool(val)
 	}
-	value = checkDeprecatedEnvVar("FEATURES_DNS_SERVERCONFIG", "FEATURES_DNS_SERVER_CONFIG")
-	if value != "" {
-		cfg.Features.DNS.ServerConfig, _ = strconv.ParseBool(value)
+	if val, ok := checkDeprecatedEnvVar("FEATURES_DNS_SERVERCONFIG", "FEATURES_DNS_SERVER_CONFIG"); ok {
+		cfg.Features.DNS.ServerConfig, _ = strconv.ParseBool(val)
 	}
 
 	if cfg.Replica != nil {
-		value = checkDeprecatedEnvVar("REPLICA_WEBURL", "REPLICA_WEB_URL")
-		if value != "" {
-			cfg.Replica.WebURL = value
+		if val, ok := checkDeprecatedEnvVar("REPLICA_WEBURL", "REPLICA_WEB_URL"); ok {
+			cfg.Replica.WebURL = val
 		}
-		value = checkDeprecatedEnvVar("REPLICA_AUTOSETUP", "REPLICA_AUTO_SETUP")
-		if value != "" {
-			cfg.Replica.AutoSetup, _ = strconv.ParseBool(value)
+		if val, ok := checkDeprecatedEnvVar("REPLICA_AUTOSETUP", "REPLICA_AUTO_SETUP"); ok {
+			cfg.Replica.AutoSetup, _ = strconv.ParseBool(val)
 		}
-		value = checkDeprecatedEnvVar("REPLICA_INTERFACENAME", "REPLICA_INTERFACE_NAME")
-		if value != "" {
-			cfg.Replica.InterfaceName = value
+		if val, ok := checkDeprecatedEnvVar("REPLICA_INTERFACENAME", "REPLICA_INTERFACE_NAME"); ok {
+			cfg.Replica.InterfaceName = val
 		}
-		value = checkDeprecatedEnvVar("REPLICA_DHCPSERVERENABLED", "REPLICA_DHCP_SERVER_ENABLED")
-		if value != "" {
-			if b, err := strconv.ParseBool(value); err != nil {
+		if val, ok := checkDeprecatedEnvVar("REPLICA_DHCPSERVERENABLED", "REPLICA_DHCP_SERVER_ENABLED"); ok {
+			if b, err := strconv.ParseBool(val); err != nil {
 				cfg.Replica.DHCPServerEnabled = utils.Ptr(b)
 			}
 		}
 	}
 }
 
-func checkDeprecatedEnvVar(oldName string, newName string) string {
+func checkDeprecatedEnvVar(oldName string, newName string) (string, bool) {
 	old, oldOK := os.LookupEnv(oldName)
 	if oldOK {
 		logger.With("deprecated", oldName, "replacement", newName).
@@ -215,44 +211,68 @@ func checkDeprecatedEnvVar(oldName string, newName string) string {
 	}
 	new, newOK := os.LookupEnv(newName)
 	if newOK {
-		return new
+		return new, true
 	}
-	return old
+	return old, oldOK
 }
 
-func checkDeprecatedReplicaEnvVar(oldPattern, newPattern, replicaID string) string {
+func checkDeprecatedReplicaEnvVar(oldPattern string, newPattern string, replicaID int) (string, bool) {
 	return checkDeprecatedEnvVar(fmt.Sprintf(oldPattern, replicaID), fmt.Sprintf(newPattern, replicaID))
 }
 
 // Manually collect replicas from env.
-func collectEnvReplicas() []types.AdGuardInstance {
+func enrichReplicasFromEnv(initialReplicas []types.AdGuardInstance) []types.AdGuardInstance {
 	var replicas []types.AdGuardInstance
 	for _, v := range os.Environ() {
 		if envReplicasURLPattern.MatchString(v) {
 			sm := envReplicasURLPattern.FindStringSubmatch(v)
-			index := sm[1]
-			re := types.AdGuardInstance{
-				URL:                sm[2],
-				WebURL:             os.Getenv(fmt.Sprintf("REPLICA%s_WEB_URL", index)),
-				APIPath:            checkDeprecatedReplicaEnvVar("REPLICA%s_APIPATH", "REPLICA%s_API_PATH", index),
-				Username:           os.Getenv(fmt.Sprintf("REPLICA%s_USERNAME", index)),
-				Password:           os.Getenv(fmt.Sprintf("REPLICA%s_PASSWORD", index)),
-				Cookie:             os.Getenv(fmt.Sprintf("REPLICA%s_COOKIE", index)),
-				InsecureSkipVerify: strings.EqualFold(checkDeprecatedReplicaEnvVar("REPLICA%s_INSECURESKIPVERIFY", "REPLICA%s_INSECURE_SKIP_VERIFY", index), "true"),
-				AutoSetup:          strings.EqualFold(checkDeprecatedReplicaEnvVar("REPLICA%s_AUTOSETUP", "REPLICA%s_AUTO_SETUP", index), "true"),
-				InterfaceName:      checkDeprecatedReplicaEnvVar("REPLICA%s_INTERFACENAME", "REPLICA%s_INTERFACE_NAME", index),
+			index, _ := strconv.Atoi(sm[1])
+			if index > len(initialReplicas) {
+				replicas = append(replicas, types.AdGuardInstance{URL: sm[2]})
+			} else {
+				re := initialReplicas[index-1]
+				re.URL = sm[2]
+				replicas = append(replicas, re)
 			}
+		}
+	}
+	for i := range replicas {
+		reID := i + 1
 
-			dhcpEnabled := checkDeprecatedReplicaEnvVar("REPLICA%s_DHCPSERVERENABLED", "REPLICA%s_DHCP_SERVER_ENABLED", index)
+		if val, ok := os.LookupEnv(fmt.Sprintf("REPLICA%d_WEB_URL", reID)); ok {
+			replicas[i].WebURL = val
+		}
+		if val, ok := os.LookupEnv(fmt.Sprintf("REPLICA%d_USERNAME", reID)); ok {
+			replicas[i].Username = val
+		}
+		if val, ok := os.LookupEnv(fmt.Sprintf("REPLICA%d_PASSWORD", reID)); ok {
+			replicas[i].Password = val
+		}
+		if val, ok := os.LookupEnv(fmt.Sprintf("REPLICA%d_COOKIE", reID)); ok {
+			replicas[i].Cookie = val
+		}
+		if val, ok := checkDeprecatedReplicaEnvVar("REPLICA%d_APIPATH", "REPLICA%d_API_PATH", reID); ok {
+			replicas[i].APIPath = val
+		}
+		if val, ok := checkDeprecatedReplicaEnvVar("REPLICA%d_INSECURESKIPVERIFY", "REPLICA%d_INSECURE_SKIP_VERIFY", reID); ok {
+			replicas[i].InsecureSkipVerify = strings.EqualFold(val, "true")
+		}
+		if val, ok := checkDeprecatedReplicaEnvVar("REPLICA%d_AUTOSETUP", "REPLICA%d_AUTO_SETUP", reID); ok {
+			replicas[i].AutoSetup = strings.EqualFold(val, "true")
+		}
+		if val, ok := checkDeprecatedReplicaEnvVar("REPLICA%d_INTERFACENAME", "REPLICA%d_INTERFACE_NAME", reID); ok {
+			replicas[i].InterfaceName = val
+		}
+
+		if dhcpEnabled, ok := checkDeprecatedReplicaEnvVar("REPLICA%d_DHCPSERVERENABLED", "REPLICA%d_DHCP_SERVER_ENABLED", reID); ok {
 			if strings.EqualFold(dhcpEnabled, "true") {
-				re.DHCPServerEnabled = utils.Ptr(true)
+				replicas[i].DHCPServerEnabled = utils.Ptr(true)
 			} else if strings.EqualFold(dhcpEnabled, "false") {
-				re.DHCPServerEnabled = utils.Ptr(false)
+				replicas[i].DHCPServerEnabled = utils.Ptr(false)
 			}
-			if re.APIPath == "" {
-				re.APIPath = "/control"
-			}
-			replicas = append(replicas, re)
+		}
+		if replicas[i].APIPath == "" {
+			replicas[i].APIPath = "/control"
 		}
 	}
 
