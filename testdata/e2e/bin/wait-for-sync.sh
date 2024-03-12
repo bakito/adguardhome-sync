ecf863948e803b58852c81715c8f69449d3e2152
bakito <github@bakito.ch>
2024-03-12 19:08:54 +0100

    fetch metrics in e2e test

--- testdata/e2e/bin/wait-for-sync.sh
+++ testdata/e2e/bin/wait-for-sync.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-kubectl wait --for=jsonpath='{.status.phase}'=Succeeded pod/adguardhome-sync --timeout=1m
-RESULT=$?
-if [[ "${RESULT}" != "0" ]]; then
-  kubectl logs adguardhome-sync
-fi
-exit ${RESULT}
+kubectl wait --for=jsonpath='{.status.phase}'=Running pod/adguardhome-sync --timeout=1m
+
+kubectl port-forward pod/adguardhome-sync 9090:9090 &
+
+for i in {1..6}; do
+  sleep 10
+   RUNNING=$(curl http://localhost:9090/api/v1/status -s | jq -r .syncRunning)
+   echo "SyncRunning = ${RUNNING}"
+   if [[ "${RUNNING}" == "false" ]]; then
+     exit 0
+   fi
+done
