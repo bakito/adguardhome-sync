02bde0eded53a1c2dcf54213f9a6a707b4378ec1
Marc Brugger <github@bakito.ch>
2024-04-04 12:21:39 +0200

    Update show-sync-metrics.sh

--- testdata/e2e/bin/show-sync-metrics.sh
+++ testdata/e2e/bin/show-sync-metrics.sh
@@ -7,5 +7,5 @@ sleep 30
 
 echo "## Pod adguardhome-sync metrics" >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
-curl ${1}://localhost:9090/metrics -s >> $GITHUB_STEP_SUMMARY
+curl ${1}://localhost:9090/metrics -k -s >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
