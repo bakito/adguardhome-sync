a32f0809a382c92ceb5a02ace6ce44874ebd91c9
Marc Brugger <github@bakito.ch>
2024-04-04 12:21:55 +0200

    Update wait-for-sync.sh

--- testdata/e2e/bin/wait-for-sync.sh
+++ testdata/e2e/bin/wait-for-sync.sh
@@ -6,7 +6,7 @@ kubectl port-forward pod/adguardhome-sync 9090:9090 &
 
 for i in {1..6}; do
   sleep 10
-   RUNNING=$(curl ${1}://localhost:9090/api/v1/status -s | jq -r .syncRunning)
+   RUNNING=$(curl ${1}://localhost:9090/api/v1/status -s -k | jq -r .syncRunning)
    echo "SyncRunning = ${RUNNING}"
    if [[ "${RUNNING}" == "false" ]]; then
      exit 0
