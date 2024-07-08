3625395d321c544bf2adf4563f30a6c81eb123b8
Marc Brugger <github@bakito.ch>
2024-07-08 21:23:08 +0200

    print e2e logs (#365)
    * keep origin pre and post logs
    
    * print origin pre and post logs

--- testdata/e2e/bin/show-origin-logs.sh
+++ testdata/e2e/bin/show-origin-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "## Pod adguardhome-origin logs" >> $GITHUB_STEP_SUMMARY
+echo "## Pod adguardhome-origin ${1} logs" >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
 kubectl logs adguardhome-origin >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
