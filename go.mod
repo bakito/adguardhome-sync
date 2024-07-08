9af8c00bca868e0317eef53f3254178eef2e363b
Marc Brugger <github@bakito.ch>
2024-07-08 20:58:34 +0200

    fix e2e test for newes agh version (#364)

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/bakito/adguardhome-sync
 
-go 1.22.0
-
-toolchain go1.22.2
+go 1.22.4
 
 require (
 	github.com/caarlos0/env/v10 v10.0.0
--- testdata/e2e/bin/install-chart.sh
+++ testdata/e2e/bin/install-chart.sh
@@ -7,3 +7,4 @@ if [[ $(helm list --no-headers -n agh-e2e | grep agh-e2e | wc -l) == "1" ]]; the
   helm delete agh-e2e -n agh-e2e --wait
 fi
 helm install agh-e2e testdata/e2e -n agh-e2e --create-namespace --set mode=${1}
+
